ed072747aa94b1641c10704ec966e15ea1da8ff1
eAsydvA <130033819+eAsydvA@users.noreply.github.com>
2025-01-14 12:41:21 +0300

    Add files via upload

---
+++ backend/middlewares/authDoctor.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+
+// doctor auth middleware
+
+const authDoctor = async (req, res, next) => {
+    try{
+
+        const {dtoken} = req.headers
+        if(!dtoken) {
+            return res.json({success:false, message: 'Не авторизован. Войти снова'})
+        }
+        const token_decode = jwt.verify(dtoken,process.env.JWT_SECRET)
+
+        // if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+        //     return res.json({success:false, message: 'Not Authorized Login Again'})
+        // }
+
+        req.body.docId = token_decode.id
+
+        next()
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+export default authDoctor
